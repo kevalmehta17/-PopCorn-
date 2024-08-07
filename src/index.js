85b98d1d60ec5f26ba84fb27fca57289f086d07a
Keval <kevalmehta885@gmail.com>
2024-08-07 08:35:52 +0530

    Added Full and Empty star Effect

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    {/* <StarRating maxRating={5} /> */}
+    {/* <StarRating maxRating={10} /> */}
     <StarRating />
   </React.StrictMode>
 );
