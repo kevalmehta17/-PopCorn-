3d972431a671de8e1d0342a089c5b6dc18efba1d
Keval <kevalmehta885@gmail.com>
2024-08-13 18:52:04 +0530

    Added API for Data fetching

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
 
-function Test() {
-  //For testing Ui right now
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" OnSetRating={setMovieRating} />
-      <p>This movie is rated {movieRating} stars </p>
-    </div>
-  );
-}
+// function Test() {
+//   //For testing Ui right now
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       {/* <App /> */}
+//       <StarRating color="blue" OnSetRating={setMovieRating} />
+//       <p>This movie is rated {movieRating} stars </p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <StarRating maxRating={5} /> */}
     {/* <StarRating maxRating={10} /> */}
-    <StarRating
+    {/* <StarRating
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={4}
     />
     <StarRating />
-    <Test></Test>
+    <Test></Test> */}
   </React.StrictMode>
 );
