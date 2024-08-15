c18071e39d26037a9951402c3e1611f58a382e29
Keval <kevalmehta885@gmail.com>
2024-08-16 00:02:11 +0530

    Added Abortcontroller for cleanup function to cancel multiple request at the same time

--- src/App.js
+++ src/App.js
@@ -82,21 +82,27 @@ export default function App() {
   }
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setISLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
           if (!res.ok)
             throw new Error("Something went wrong to fetch the Movie Data");
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movie not Found");
           setMovies(data.Search);
+          setError("");
         } catch (error) {
           setError(error.message); // Set error message to state
           console.error("Error fetching movies:", error); // Print error to console
+          if (error.name !== "AbortError") {
+            setError(error.message);
+          }
         } finally {
           //this help to remove rendering Loading, if the Error is available
           setISLoading(false);
@@ -108,6 +114,9 @@ export default function App() {
         return;
       }
       fetchMovies();
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
