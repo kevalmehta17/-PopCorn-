9309131d9a094773b8f3555a1c20c627174167cc
Keval <kevalmehta885@gmail.com>
2024-08-15 19:12:54 +0530

    Added the new state so if ther is a rating then u can add movie to the list

--- src/App.js
+++ src/App.js
@@ -257,6 +257,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
 
   const [movie, setMovie] = useState({});
   const [isLoading, setISLoading] = useState(false);
+  const [userRating, setUserRating] = useState(null);
 
   function handleAdd() {
     const newWatchedMovie = {
@@ -266,8 +267,10 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
       poster,
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split("").at(0)),
+      userRating,
     };
     onAddWatched(newWatchedMovie);
+    onCloseMovie();
   }
 
   useEffect(
@@ -330,10 +333,16 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
           <section>
             <div className="rating">
               {" "}
-              <StarRating maxRating={10} size={24} />
-              <button className="btn-add" onClick={handleAdd}>
-                +Add to list
-              </button>
+              <StarRating
+                maxRating={10}
+                size={24}
+                OnSetRating={setUserRating}
+              />
+              {userRating > 0 && (
+                <button className="btn-add" onClick={handleAdd}>
+                  +Add to list
+                </button>
+              )}
             </div>
 
             <p>
